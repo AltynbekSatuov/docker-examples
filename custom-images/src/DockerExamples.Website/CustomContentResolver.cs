3711980744c15e670ccb9a39838ccbf490dc9530
altynbek <altynbek@example.com>
2025-02-14 22:05:17 +0500

    Task 9: Add custom content resolver

---
+++ custom-images/src/DockerExamples.Website/CustomContentResolver.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.LayoutService.Configuration;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace DockerExamples.Website
+{
+    public class CustomContentResolver : Sitecore.LayoutService.ItemRendering.ContentsResolvers.IRenderingContentsResolver
+    {
+        public bool IncludeServerUrlInMediaUrls { get; set; }
+        public bool UseContextItem { get; set; }
+        public string ItemSelectorQuery { get; set; }
+        public NameValueCollection Parameters { get; set; }
+        public object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            //if you want to access the datasource item
+            var customSettingsValue = Sitecore.Configuration.Settings.GetSetting("CustomSitecoreSetting");
+
+            return new
+            {
+                customSettings = customSettingsValue,
+                date = DateTime.Now,
+            };
+        }
+    }
+}
